105a5f7b20b071e2f2f145687293be84fbaa33b7
OliviaShi <yas089@ucsd.edu>
2019-07-31 13:58:36 +0800

    olivia first wxss

--- pages/show/show.js
+++ pages/show/show.js
@@ -5,7 +5,8 @@ Page({
    * Page initial data
    */
   data: {
-    times: ["8:00","9:00","10:00","11:00"],
+    times: ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00","17:00","18:00"],
+
     currentDate: new Date().getTime(),
     minDate: new Date().getTime(),
     formatter(type, value) {

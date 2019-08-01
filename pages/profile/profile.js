bea728cfcb6308f5d6161886c33dd01a0cc9d88c
OliviaShi <yas089@ucsd.edu>
2019-08-01 22:44:07 +0800

    forgot to push my code + starting on profile css

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,26 +1,89 @@
 
-
+const app = getApp()
 Page({
   data: {
     time: "",
-    date: ""
+    date: "",
+    userInfo: {},
+    hasUserInfo: false,
+    userId: '',
+    hasRegistered: false,
+    user: ""
+  },
+
+  onLoad: function () {
+    let userId = app.globalData.userId
+    const page = this
+    page.setData({ userId: userId })
+    wx.request({
+      url: `${app.globalData.url}users/${userId}`,
+      method: 'GET',
+      success(res) {
+        let user = res.data
+        page.setData({ user: user })
+        if (res.data.email != null) {
+          page.setData({ hasRegistered: true })
+        }
+      }
+    })
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    }
+
+
   },
 
   bindTimeChange(e) {
-  let { value } = e.detail;
-  console.log("time:", value);
-  this.setData({
-    time: value
-  })
-},
+    let { value } = e.detail;
+    console.log("time:", value);
+    this.setData({
+      time: value
+    })
+  },
 
-bindDateChange(e) {
-  let { value } = e.detail;
-  console.log("date:", value);
-  this.setData({
-    date: value
-  })
-}
+  bindDateChange(e) {
+    let { value } = e.detail;
+    console.log("date:", value);
+    this.setData({
+      date: value
+    })
+  },
+
+  getUserInfo: function (e) {
+    app.globalData.userInfo = e.detail.userInfo
+    // wx.request({
+    //   url: `${app.globalData.url}services`,
+    // })
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true,
+      userId: app.globalData.userId,
+      userInfo: app.globalData.userInfo
+    })
+  },
+
+  formSubmit: function (e) {
+    let id = this.data.userId
+    let name = e.detail.value.name
+    let description = e.detail.value.description
+    let email = e.detail.value.email
+    let user = { name: name, description: description, email: email }
+    wx.request({
+      url: `${app.globalData.url}users/${id}`,
+      method: 'PUT',
+      data: user,
+      success: function (res) {
+        console.log(res)
+        wx.reLaunch({
+          url: '/pages/profile/profile',
+        })
+      }
+    })
+  },
 
 
 })
